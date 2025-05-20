8874df3b116c5f5ef1ffce9da87b60d0ae00d8db
hogakubuyametai <hornet1115@gmal.com>
2025-05-20 22:46:17 +0900

    feat: artistIdをキーとしてartistImageUrlとtopTracksをキャッシュする機能の実装

---
+++ gigdig/composables/useArtistCache.ts
@@ -0,0 +1,42 @@
+import { getArtistDetails } from '@/utils/spotify';
+import { getArtistTopTracks } from '@/utils/spotify';
+
+const gigDataCache = new Map<string, {artistImageUrl: string, topTracks: any[]}>();
+
+export function useArtistCache() {
+  async function getArtistData (artistId: string) {
+    if (gigDataCache.has(artistId)) {
+      return {
+        artistImageUrl: gigDataCache.get(artistId)?.artistImageUrl,
+        topTracks: gigDataCache.get(artistId)?.topTracks,
+      };
+    }
+    const artistDetails = await getArtistDetails(artistId);
+    const artistImageUrl = artistDetails?.images[0]?.url || '';
+    const topTracks = await getArtistTopTracks(artistId) || [];
+
+    if (!artistImageUrl) {
+      throw new Error('No artist image found');
+    }
+    if (!topTracks) {
+      throw new Error('No top tracks found');
+    }
+
+    setArtistData(artistId, artistImageUrl, topTracks);
+
+    return {
+      artistImageUrl,
+      topTracks,
+    };
+  }
+
+  function setArtistData(artistId: string, artistImageUrl: string, topTracks: any[]) {
+    gigDataCache.set(artistId, { artistImageUrl, topTracks });
+  }
+
+  return {
+    getArtistData,
+    setArtistData,
+  };
+}
+
